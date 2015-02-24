841a6677de2b905358ab1ae50b8155fc341c503e
Dave Kessler <activethrasher00@gmail.com>
2015-02-24 02:07:45 -0500

    exodus_config: FDO cleanup
    Change-Id: I9f99ce6a692207d5cd43c406b28a94e385a7bf50

--- core/combo/fdo.mk
+++ core/combo/fdo.mk
@@ -22,10 +22,10 @@ ifneq ($(wildcard $(DEVICE_PROFILE)),)
   CURRENT_DATE := $(shell date '+%d')
   RESET_DATE := 01
   ifeq ($(CURRENT_DATE),$(RESET_DATE))
-    $(shell @echo rm -rf /.exodus_profiles)
     $(info "=========================================================================")
     $(info "=======================RESETTING PROFILING DATA!=========================")
     $(info "=========================================================================")
+    $(shell @echo rm -rf /.exodus_profiles)
   endif
 endif
 
@@ -62,32 +62,30 @@ ifeq ($(USE_FDO_OPTIMIZATION),true)
         -fprofile-use=$(DEVICE_PROFILE) \
         $(SAMPLE_PROFILING_FLAGS)
   endif
-
 else
+  # Clean up profiles we've generated now that FDO is off
+  ifneq ($(wildcard $(DEVICE_PROFILE)),)
+    $(shell @echo rm -rf /.exodus_profiles)
+  endif
 
-# Clean up profiles we've generated now that FDO is off
-ifneq ($(wildcard $(DEVICE_PROFILE)),)
-  $(shell @echo rm -rf /.exodus_profiles)
-endif
+  # Begin AOSP's FDO implementation.  This can only be used when USE_FDO_OPTIMIZATION is not set/false.
+  ifeq ($(strip $(BUILD_FDO_INSTRUMENT)), true)
+    # Set BUILD_FDO_INSTRUMENT=true to turn on FDO instrumentation.
+    # The profile will be generated on /sdcard/fdo_profile on the device.
+    $(combo_2nd_arch_prefix)TARGET_FDO_CFLAGS := -fprofile-generate=/sdcard/fdo_profile -DANDROID_FDO
+    $(combo_2nd_arch_prefix)TARGET_FDO_LDFLAGS := -lgcov -lgcc
+  else
+    ifeq ($(strip $(BUILD_FDO_OPTIMIZE)), true)
+      # Set TARGET_FDO_PROFILE_PATH to set a custom profile directory for your build.
+      ifeq ($(strip $($(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH)),)
+        $(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH := vendor/google_data/fdo_profile
+      endif
 
-# AOSP's FDO implementation
-ifeq ($(strip $(BUILD_FDO_INSTRUMENT)), true)
-  # Set BUILD_FDO_INSTRUMENT=true to turn on FDO instrumentation.
-  # The profile will be generated on /sdcard/fdo_profile on the device.
-  $(combo_2nd_arch_prefix)TARGET_FDO_CFLAGS := -fprofile-generate=/sdcard/fdo_profile -DANDROID_FDO
-  $(combo_2nd_arch_prefix)TARGET_FDO_LDFLAGS := -lgcov -lgcc
-else
-  ifeq ($(strip $(BUILD_FDO_OPTIMIZE)), true)
-    # Set TARGET_FDO_PROFILE_PATH to set a custom profile directory for your build.
-    ifeq ($(strip $($(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH)),)
-      $(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH := vendor/google_data/fdo_profile
-    endif
-
-    ifneq ($(strip $(wildcard $($(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH)/$(PRODUCT_OUT))),)
-      $(combo_2nd_arch_prefix)TARGET_FDO_CFLAGS := -fprofile-use=$($(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH) -DANDROID_FDO -fprofile-correction -Wcoverage-mismatch -Wno-error
-    else
-      $(warning Profile directory $($(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH)/$(PRODUCT_OUT) does not exist. Turn off FDO.)
+      ifneq ($(strip $(wildcard $($(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH)/$(PRODUCT_OUT))),)
+        $(combo_2nd_arch_prefix)TARGET_FDO_CFLAGS := -fprofile-use=$($(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH) -DANDROID_FDO -fprofile-correction -Wcoverage-mismatch -Wno-error
+      else
+        $(warning Profile directory $($(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH)/$(PRODUCT_OUT) does not exist. Turn off FDO.)
+      endif
     endif
   endif
-endif
 endif
